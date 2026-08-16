d194122a82f7b0b9d4149e16e20bc2f89e4efdd8
mw1435 <mw1435@gmail.com>
2026-08-16 21:24:35 +0100

    Change header title to 'Last Person Standing Payments'

--- src/App.jsx
+++ src/App.jsx
@@ -177,7 +177,7 @@ export default function App() {
   return (
     <div className="container">
       <div className="header">
-        <h1>Entrants</h1>
+        <h1>Last Person Standing Payments</h1>
         <button className="btn-small" onClick={signOut}>Sign out</button>
       </div>
 
