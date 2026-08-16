f5e51e37fcf3d3792691e16eb91f5dc20843794c
mw1435 <mw1435@gmail.com>
2026-08-16 21:16:58 +0100

    Fix missing newline at end of App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ export default function App() {
       setAuthStep('dashboard');
       loadEntrants(saved);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const jsonpRequest = (url, params) => {
